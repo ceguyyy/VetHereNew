9659fa57385b6f32ff1879738290812d2ab5e8ef
Christian Gunawan <cg404cgunawan@gmail.com>
2024-12-04 18:13:34 +0700

    Add: Add tabbar on contentView

---
+++ VetHere-ios-clone/Features/History/View/HistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryView.swift
+//  VetHere-ios-clone
+//
+//  Created by Christian Gunawan on 04/12/24.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoryView()
+}
---
+++ VetHere-ios-clone/Features/MyPet/View/MyPetView.swift
@@ -0,0 +1,18 @@
+//
+//  MyPet.swift
+//  VetHere-ios-clone
+//
+//  Created by Christian Gunawan on 04/12/24.
+//
+
+import SwiftUI
+
+struct MyPet: View {
+    var body: some View {
+        Text("My Pet")
+    }
+}
+
+#Preview {
+    MyPet()
+}
