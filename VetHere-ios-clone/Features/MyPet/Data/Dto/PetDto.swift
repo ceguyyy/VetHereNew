397424018ada386f9695bfda2bdc022bf93a3dc3
Andrew Oroh <andreworoh.ao@gmail.com>
2024-12-08 14:42:42 +0700

    Add : Add vaccine, medical record, medical record detail, and fix pet model

--- VetHere-ios-clone/Features/MyPet/Data/Dto/PetDto.swift
+++ VetHere-ios-clone/Features/MyPet/Data/Dto/PetDto.swift
@@ -13,3 +13,20 @@ struct GetUserPetsResponseDto : Codable{
     let pet_type : String?
     let pet_image : String?
 }
+
+struct GetPetDetailRequestDto : Codable{
+    let pet_id : String
+}
+
+struct GetPetDetailResponseDto : Codable{
+    let pet_id : String
+    let pet_type : String?
+    let pet_breed : String?
+    let pet_image: String?
+    let pet_weight : Double?
+    let pet_name:String
+    let pet_color:String?
+    let pet_dob:String?
+    let medical_record : MedicalHistorylResponseDto?
+    let vaccine_histories : [VaccineHistorylResponseDto]?
+}
--- VetHere-ios-clone/Features/MyPet/Data/Models/Pet.swift
+++ VetHere-ios-clone/Features/MyPet/Data/Models/Pet.swift
@@ -12,11 +12,24 @@ class Pet : Identifiable{
     let petName : String
     let petType : String?
     let petImage : String?
+    let petWeight : Double?
+    let petDOB : String?
+    let petBreed : String?
+    let petColor : String?
+    let medicalRecord : MedicalRecord?
+    let vaccineHistory : [VaccineHistory]
     
-    init(petId: String, petName: String, petType: String, petImage: String) {
+    init(petId: String, petName: String, petType: String?, petImage: String?, petWeight:Double?, petDOB: String?, petBreed: String?, petColor: String?, medicalRecord : MedicalRecord?, vaccineHistory : [VaccineHistory]) {
         self.petId = petId
         self.petName = petName
         self.petType = petType
+        self.petWeight = petWeight
         self.petImage = petImage
+        self.petDOB = petDOB
+        self.petBreed = petBreed
+        self.petColor = petColor
+        self.medicalRecord = medicalRecord
+        self.vaccineHistory = vaccineHistory
+        
     }
 }
